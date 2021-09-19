a1450f8f948148dd1972c41e1b8cda775279fcd0
Roman Serebrov <romanspb00@mail.ru>
2021-09-19 23:06:06 +0300

    add

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,35 +1,35 @@
-// // import { NotImplementedError } from '../extensions/index.js';
+import { NotImplementedError } from '../extensions/index.js';
 
-// // const MODERN_ACTIVITY = 15;
-// // const HALF_LIFE_PERIOD = 5730;
+// const MODERN_ACTIVITY = 15;
+// const HALF_LIFE_PERIOD = 5730;
 
-// // /**
-// //  * Determine the age of archeological find by using
-// //  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
-// //  * 
-// //  * @param {String} sampleActivity string representation of current activity 
-// //  * @return {Number | Boolean} calculated age in years or false
-// //  * in case of incorrect sampleActivity
-// //  *
-// //  * @example
-// //  * 
-// //  * dateSample('1') => 22387
-// //  * dateSample('WOOT!') => false
-// //  *
-// //  */
-// export default  function dateSample(sampleActivity) {
-//   if(!sampleActivity || typeof sampleActivity !== 'string' || Number(sampleActivity)>15 || Number(sampleActivity) <= 0) {
-//     return false
-//   }
-//   sampleActivity = Number(sampleActivity)
+// /**
+//  * Determine the age of archeological find by using
+//  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
+//  * 
+//  * @param {String} sampleActivity string representation of current activity 
+//  * @return {Number | Boolean} calculated age in years or false
+//  * in case of incorrect sampleActivity
+//  *
+//  * @example
+//  * 
+//  * dateSample('1') => 22387
+//  * dateSample('WOOT!') => false
+//  *
+//  */
+export default function dateSample(sampleActivity) {
+  if(!sampleActivity || typeof sampleActivity !== 'string' || Number(sampleActivity)>15 || Number(sampleActivity) <= 0) {
+    return false
+  }
+  sampleActivity = Number(sampleActivity)
   
-//   if(isNaN(sampleActivity) || !isFinite(sampleActivity)) {
-//     return false
-//   }
+  if(isNaN(sampleActivity) || !isFinite(sampleActivity)) {
+    return false
+  }
   
    
-//   const MODERN_ACTIVITY = 15;
-//   const HALF_LIFE_PERIOD = 5730;
+  const MODERN_ACTIVITY = 15;
+  const HALF_LIFE_PERIOD = 5730;
   
-//   return  Math.ceil( Math.log( MODERN_ACTIVITY / sampleActivity ) / (0.693 / HALF_LIFE_PERIOD) )
-// }
+  return  Math.ceil( Math.log( MODERN_ACTIVITY / sampleActivity ) / (0.693 / HALF_LIFE_PERIOD) )
+}
